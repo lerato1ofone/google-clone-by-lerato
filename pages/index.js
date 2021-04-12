e8e8a73384bdc1cbaa7ed2899e5075a2451f5df1
lerato1ofone <letsepe100@gmail.com>
2021-04-12 20:26:46 +0200

    Added header.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,39 @@
 import Head from 'next/head'
+import Avatar from '../components/Avatar'
+import {Microphone, ViewGridIcon} from '@heroicons/react/solid'
 
 export default function Home() {
   return (
     <div className="">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Google</title>
+        <link rel="icon" href="/Google-Chrome-Google-Chrome.ico" />
       </Head>
 
-     <h1>Building Goodle with Next.js and Tailwind css</h1>
+      {/* Header */}
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+      {/* Left side */}
+        <div className="flex space-x-4 items-center">
+        <p className="link">About</p>
+        <p className="link">Store</p>
+        </div>
+
+        {/* Right side */}
+        <div className="flex space-x-4 items-center">
+        <p className="link">Gmail</p>
+        <p className="link">Images</p>
+       
+
+        {/* Icon */}
+        <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+
+        <Avatar url="https://www.pngfind.com/pngs/m/676-6764065_default-profile-picture-transparent-hd-png-download.png" />
+        </div>
+
+        </header>
+      {/* Body */}
+
+      {/* Footer */}
     </div>
   )
 }
