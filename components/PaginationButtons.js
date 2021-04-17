5b718d2a57398facec143e0ed43f3e9a020f70e0
lerato1ofone <letsepe100@gmail.com>
2021-04-17 23:55:30 +0200

    Added search type query on PaginationButtons

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -8,10 +8,17 @@ function PaginationButtons() {
 
     const startIndex = Number(router.query.start) || 0;
 
+    const searchType = router.query.searchType == 'web' ? null : router.query.searchType;
+
+    let query;
+
+    if(searchType)
+        query = `&searchType=${searchType}`;
+
     return (
         <div className="flex max-w-lg justify-between text-blue-700 mb-10">
             {startIndex >= 10 && (
-                <Link href={`/search?term=${router.query.term}&start=${startIndex-10}`}>
+                <Link href={`/search?term=${router.query.term}&${query ? `${query}` : ``}&start=${startIndex-10}`}>
                    <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
                         <ChevronLeftIcon className="h-5"/>
                         <p>Previous</p>
@@ -19,7 +26,7 @@ function PaginationButtons() {
                 </Link>
             )}
             
-            <Link href={`/search?term=${router.query.term}&start=${startIndex+10}`}>
+            <Link href={`/search?term=${router.query.term}&${query ? `${query}` : ``}&start=${startIndex+10}`}>
                 <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
                     <ChevronRightIcon className="h-5"/>
                     <p>Next</p>
